582bfe9d15050063ea0eafe5409f399e2c95566e
Konstantin Anisimov <anisimovkosta19@gmail.com>
2026-05-14 01:34:55 +0300

    add evil mc into emacs

--- emacs/.config/emacs/lisp/my-evil.el
+++ emacs/.config/emacs/lisp/my-evil.el
@@ -22,6 +22,40 @@
   :config
   (evil-collection-init))
 
+(use-package evil-mc
+  :after evil
+  :commands (evil-mc-make-and-goto-next-match
+             evil-mc-make-and-goto-prev-match
+             evil-mc-make-all-cursors
+             evil-mc-make-cursor-move-next-line
+             evil-mc-make-cursor-move-prev-line
+             evil-mc-skip-and-goto-next-match
+             evil-mc-skip-and-goto-prev-match
+             evil-mc-undo-last-added-cursor
+             evil-mc-undo-all-cursors
+             evil-mc-pause-cursors
+             evil-mc-resume-cursors)
+  :bind (:map evil-normal-state-map
+              ("C-c m n" . evil-mc-make-and-goto-next-match)
+              ("C-c m p" . evil-mc-make-and-goto-prev-match)
+              ("C-c m a" . evil-mc-make-all-cursors)
+              ("C-c m j" . evil-mc-make-cursor-move-next-line)
+              ("C-c m k" . evil-mc-make-cursor-move-prev-line)
+              ("C-c m N" . evil-mc-skip-and-goto-next-match)
+              ("C-c m P" . evil-mc-skip-and-goto-prev-match)
+              ("C-c m u" . evil-mc-undo-last-added-cursor)
+              ("C-c m U" . evil-mc-undo-all-cursors)
+              ("C-c m SPC" . evil-mc-pause-cursors)
+              ("C-c m RET" . evil-mc-resume-cursors)
+         :map evil-visual-state-map
+              ("C-c m n" . evil-mc-make-and-goto-next-match)
+              ("C-c m p" . evil-mc-make-and-goto-prev-match)
+              ("C-c m a" . evil-mc-make-all-cursors))
+  :config
+  (global-evil-mc-mode 1)
+  (with-eval-after-load 'which-key
+    (which-key-add-key-based-replacements "C-c m" "evil-mc")))
+
 (use-package god-mode
   :after evil
   :init
